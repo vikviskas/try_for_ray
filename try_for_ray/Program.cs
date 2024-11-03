3f4b844bbbb2e25c0b4cfbc308734ded8b0cce27
User <User@DESKTOP-MPPNFI0>
2024-11-03 18:52:49 +0500

    Добавьте файлы проекта.

---
+++ try_for_ray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace try_for_ray
+{
+    internal class Program
+    {
+        public interface IObject
+        {
+            object Clone();
+
+        }
+        
+    }
+}
